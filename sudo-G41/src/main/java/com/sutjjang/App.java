0fc6c463f812ca5bbb363b62e98a2a900fd26b73
sudo-G41 <sudo.G41@gmail.com>
2021-03-30 21:53:16 +0900

    ???

--- sudo-G41/src/main/java/com/sutjjang/App.java
+++ sudo-G41/src/main/java/com/sutjjang/App.java
@@ -3,6 +3,7 @@ package com.sutjjang;
 import java.util.Scanner;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 
 public class App{
 	public static void main(String[] args){
@@ -15,7 +16,7 @@ public class App{
 		// Scanner sc = new Scanner(g.getInput());
 		// System.out.println(sc.nextLine());
 		qwer qw = new qwer();
-		JCommander.newBuilder().addObject(qw).build().parse(args);
+		// JCommander.newBuilder().addObject(qw).build().parse(args);
 		System.out.println(qw.Help());
 	}
 }
